e2c93f31fc64f14bc6076f18b1c4107d087e9aff
hansCC <hans.cc@ufl.edu>
2015-11-23 19:24:01 -0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,3 +1,8 @@
-using namespace std;
+#include <string>
 
-cout << "HELLO HANS HOW ARE YOU";
+using namespace std;
+int main(){
+  cout << "HELLO HANS HOW ARE YOU";
+  cout << "Pretty bad actually."
+  return 0;
+}
